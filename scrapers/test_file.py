b611b2a14879530bd3b6831558c7f57f0ff2e602
matheusbodin <matheusbodin321@gmail.com>
2024-11-03 21:00:52 +0000

    test file

--- scrapers/test_file.py
+++ scrapers/test_file.py
@@ -1,5 +1,6 @@
 from operators.dev_library import get_driver, get_soup
 import time
+import requests
 
 # Teste do Selenium
 driver = get_driver(headless=False)  # Defina headless=True para não abrir a interface gráfica
@@ -11,3 +12,17 @@ driver.quit()
 # Teste do Beautiful Soup
 soup = get_soup("https://stackoverflow.com/questions/62131724/use-pipenv-to-build-and-publish-a-python-package")
 print("Título da página obtido pelo Beautiful Soup:", soup.title.string)
+
+# URL de uma API pública de exemplo
+url = "https://jsonplaceholder.typicode.com/todos/1"
+
+# Faz a requisição GET
+response = requests.get(url)
+
+# Verifica se a requisição foi bem-sucedida
+if response.status_code == 200:
+    # Converte a resposta para JSON
+    data = response.json()
+    print("Resposta:", data)
+else:
+    print("Erro:", response.status_code)
